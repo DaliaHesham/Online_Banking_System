df58fcc5374987f48e875b78c921a03b64653887
ebramfadl <ebram@ntsal.com>
2023-05-28 14:40:55 +0300

    prepare to deploy

--- js/user-login.js
+++ js/user-login.js
@@ -4,11 +4,11 @@ var signInBtn = document.getElementById('logIn');
 signInBtn.addEventListener('click', function() {
   var inputValue = inputBox.value;
   if (inputValue === 'admin') {
-    window.location.href = '../html/technical-reports.html'; 
+    window.location.href = './html/technical-reports.html'; 
   } else if(inputValue === 'banker'){
-    window.location.href = '../html/home-banker.html'; 
+    window.location.href = './html/home-banker.html'; 
   } else{
-    window.location.href = '../html/home.html'; 
+    window.location.href = './html/home.html'; 
 
   }
 });
