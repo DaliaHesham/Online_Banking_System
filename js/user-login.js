37e6a0ffd4e5bab9fc2be2ce1dc4c4130010f503
ebramfadl <ebram@ntsal.com>
2023-05-28 03:01:57 +0300

    all pages responsive + updates done/javascript

---
+++ js/user-login.js
@@ -0,0 +1,14 @@
+var inputBox = document.getElementById('inputEmail');
+var signInBtn = document.getElementById('logIn');
+
+signInBtn.addEventListener('click', function() {
+  var inputValue = inputBox.value;
+  if (inputValue === 'admin') {
+    window.location.href = '../html/technical-reports.html'; 
+  } else if(inputValue === 'banker'){
+    window.location.href = '../html/home-banker.html'; 
+  } else{
+    window.location.href = '../html/home.html'; 
+
+  }
+});
